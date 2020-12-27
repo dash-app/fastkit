83c1fef119fb2f267bac99fda569a6cf9b9db817
Laica Lunasys <laica.lunasys@gmail.com>
2020-12-27 12:37:37 +0900

    Add fastkit one-shot script

---
+++ fastkit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$UID" != "0" ]; then
+    echo "E: Must be root."
+    exit 1
+fi
+
+echo ""
+echo ""
+echo "*** Dash-App - Fastkit ***"
+echo "- Starting automatic Install after 3 seconds... -"
+echo ""
+echo ""
+sleep 3
+
+for f in $(find ./setup -type f -name '*.sh' | sort); do
+    echo "==> Running Setup: $f"
+    bash $f
+done
